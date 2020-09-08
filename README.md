ff943aedcf3333f5dbd1d3579aebf6b28edf1471
DeHoKo <zenkovsk@gmail.com>
2020-09-08 23:12:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ESHOP
+
+I want to try tailwind.css, so I decided to create an eshop, where I can play with this library
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="max-w-sm rounded border-2 mx-auto">Hello Nikitos</div>;
+}
+
+export default App;
